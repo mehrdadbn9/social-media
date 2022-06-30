521d3614f36aeff2bf722cf5d46585b18fce9df9
mehrdadbn9 <mehrdadbiukian@gmail.com>
2022-06-30 23:41:49 +0430

    Post got complete such as: Delete, Update and buttons

--- home/views.py
+++ home/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import View
+
+from .form import PostUpdateForm
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(View):
     def get(self, request):
-	    return render(request, 'home/index.html')
+        posts = Post.objects.all()
+        return render(request, 'home/index.html', {'posts': posts})
 
-    def post(self, request):
-	    return render(request, 'home/index.html')
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        return render(request, 'home/detail.html', {'post': post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you can"t delete this post', 'danger')
+        return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        if not post.user.id == request.user.id:
+            messages.error(request, 'you can\t update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'form': form})
+
+    def post(self, request, post_id):
+        pass
