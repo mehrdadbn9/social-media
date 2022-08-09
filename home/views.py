d3a6b97c901c9fced713524d4c08d98d52fe8e6d
mehrdadbn9 <mehrdadbiukian@gmail.com>
2022-08-09 17:02:24 +0430

    all type of comments has been added

--- home/views.py
+++ home/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
 from django.views import View
 
-from .form import PostCreateUpdateForm
-from .models import Post
+from .form import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class HomeView(View):
@@ -15,9 +17,33 @@ class HomeView(View):
 
 
 class PostDetailView(View):
-    def get(self, request, post_id, post_slug):
-        post = get_object_or_404(pk=post_id, slug=post_slug)
-        return render(request, 'home/detail.html', {'post': post})
+    # def get(self, request, post_id, post_slug):
+    #     post = get_object_or_404(pk=post_id, slug=post_slug)
+    #     comments = post.pcomments.filter(is_reply=False)
+    #     return render(request, 'home/detail.html', {'post': post, 'comments': comments})
+    form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html',
+                      {'post': self.post_instance, 'comments': comments, 'form': self.form_class,
+                       'reply_form': self.form_class_reply})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'your comment submitted successfully', 'success')
+            return redirect('home:post_detail', self.post_instance.id, self.post_instance.slug)
 
 
 class PostDeleteView(LoginRequiredMixin, View):
@@ -83,3 +109,21 @@ class PostCreateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'u created this post', 'success')
             return redirect('home:post_detail', new_post.id, new_post.slug)
+
+
+class PostAddReplyView(LoginRequiredMixin, View):
+    form_class = CommentReplyForm
+
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, id=comment_id)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'your reply submitted successfully', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
